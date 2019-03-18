49220c436b74b1252e935bbbc77b78b1ccb8981f
mizt <info@mizt.org>
2019-03-18 14:04:13 +0900

    added onResize

---
+++ onResize/onResize.mm
@@ -0,0 +1,19 @@
+#import <Cocoa/Cocoa.h>
+
+static void onResize(CGDirectDisplayID displayID, CGDisplayChangeSummaryFlags flags, void *me) {
+    if(CGMainDisplayID()==displayID) {
+        if(flags&kCGDisplayDesktopShapeChangedFlag) {            
+            NSLog(@"%@",NSStringFromRect([[[NSScreen screens] objectAtIndex:0] frame]));
+        }
+    }
+}
+    
+int main(int argc, char *argv[]) {
+
+    @autoreleasepool {
+        
+        CGDisplayRegisterReconfigurationCallback(onResize,nullptr);
+        [[NSApplication sharedApplication] run];       
+        
+    }
+}
