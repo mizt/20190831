ae898907a2e03a6e4ee18c009357871fa71b08ba
mizt <info@mizt.org>
2019-03-18 14:01:12 +0900

    added wait

---
+++ wait/wait.mm
@@ -0,0 +1,32 @@
+#import <Cocoa/Cocoa.h>
+
+class App {
+    
+    private:
+        
+        dispatch_semaphore_t semaphore = dispatch_semaphore_create(0);
+
+    public:
+        
+        App() {
+            NSLog(@"begin");
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW,NSEC_PER_SEC),dispatch_queue_create("wait",DISPATCH_QUEUE_CONCURRENT),^{                 
+                dispatch_semaphore_signal(this->semaphore);
+            });
+            dispatch_semaphore_wait(this->semaphore,DISPATCH_TIME_FOREVER);
+            NSLog(@"end");
+        }
+        
+        ~App() {
+        }
+};
+
+int main(int argc, char *argv[]) {
+
+    @autoreleasepool {
+        
+        App *app = new App();
+        [[NSApplication sharedApplication] run];       
+        
+    }
+}
