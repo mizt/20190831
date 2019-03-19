fe01dab5a2b2f97691f5096bb39bc1c21bed247c
mizt <info@mizt.org>
2019-03-19 18:33:57 +0900

    added Menu

---
+++ Menu/Menu.mm
@@ -0,0 +1,72 @@
+#import <Cocoa/Cocoa.h>
+#import "Menu.h"
+
+class App {
+    
+    private:
+        
+    public:
+        
+        App() {
+            
+             Menu::$()->on(^(id me,IMenuItem *item) {
+                if(item) {
+                    MenuType type = item->type();
+                    if(type==MenuType::TEXT) {
+                        if(item->eq(@"Quit")) {
+                            [NSApp terminate:nil];
+                        }
+                    }
+                    else if(type==MenuType::RADIOBUTTON) {
+                        if(item->eq(@"commands")) {
+                            NSLog(@"%f",item->value());
+                        }
+                    }
+                    else if(type==MenuType::CHECKBOX) {
+                        if(item->eq(@"!")) {
+                             NSLog(@"%d",(item->value()>=0.5)?1:0);
+                        }
+                    }
+                    else if(type==MenuType::SLIDER) {
+                        if(item->eq(@"~")) {
+                            NSLog(@"%f",item->value());
+                        }
+                    }
+                }
+            })->addItem(@"~",MenuType::SLIDER,@"{'value':0.8,'min':0.0,'max':1.0}")
+            ->hr()
+            ->addItem(@"commands",MenuType::RADIOBUTTON,@"{'label':[1,2,3],'value':0.0}")
+            ->hr()
+            ->addItem(@"!",MenuType::CHECKBOX,@"{'value':0.0}")
+            ->hr()
+            ->addItem(@"Quit",MenuType::TEXT,@"{'key':'q'}");
+            
+        }
+        
+        ~App() {
+        }
+};
+
+#pragma mark AppDelegate
+@interface AppDelegate:NSObject <NSApplicationDelegate> {
+    App *app;
+}
+@end
+@implementation AppDelegate
+-(void)applicationDidFinishLaunching:(NSNotification*)aNotification {
+    app = new App();
+}
+-(void)applicationWillTerminate:(NSNotification *)aNotification {
+    delete app;
+}
+@end
+
+int main(int argc, char *argv[]) {
+    @autoreleasepool {
+    
+        id app = [NSApplication sharedApplication];
+        id delegat = [AppDelegate alloc];
+        [app setDelegate:delegat];
+        [app run];
+    }
+}
---
+++ Menu/Menu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+dir=$(cd $(dirname $0)&&pwd)
+cd $dir
+
+App="Menu"
+
+mkdir -p ${App}.app/Contents/{MacOS,Resources}
+
+cat > ${App}.app/Contents/Info.plist <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+	<dict>
+		<key>CFBundleExecutable</key>
+		<string>${App}</string>
+	</dict>
+</plist>
+EOF
+
+if [ -e ${App} ]; then
+	mv ${App} ${App}.app/Contents/MacOS/
+	chmod +x ${App}.app/Contents/MacOS/${App}
+fi
