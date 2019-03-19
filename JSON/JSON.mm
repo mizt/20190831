3c6c2c89b05996feb5e2f4e2d36a07ce26b9a209
mizt <info@mizt.org>
2019-03-20 04:19:51 +0900

    updated

--- JSON/JSON.mm
+++ JSON/JSON.mm
@@ -11,18 +11,20 @@ int main(int argc, char *argv[]) {
             NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:&error];
             if(error==nil) {
                 int ch = 3;
-                if(dict[@"background"]&&[dict[@"background"] count]==ch) {                    
-                    unsigned int color = 0xFF000000;
-                    for(int k=0; k<ch; k++) {
-                        if([[dict[@"background"][k] className] compare:@"__NSCFNumber"]==NSOrderedSame) {
-                            int tmp = [dict[@"background"][k] intValue];
-                            if(tmp>0&&tmp<0x100) {
-                                color|=tmp<<(8*(ch-k-1));
+                if([[dict[@"background"] className] compare:@"__NSArrayI"]==NSOrderedSame) {
+                    if(dict[@"background"]&&[dict[@"background"] count]==ch) {                    
+                        unsigned int color = 0xFF000000;
+                        for(int k=0; k<ch; k++) {
+                            if([[dict[@"background"][k] className] compare:@"__NSCFNumber"]==NSOrderedSame) {
+                                int tmp = [dict[@"background"][k] intValue];
+                                if(tmp>0&&tmp<0x100) {
+                                    color|=tmp<<(8*(ch-k-1));
+                                }
                             }
                         }
+                        NSLog(@"0x%X",color);
                     }
-                    NSLog(@"0x%X",color);
-                }                
+                }               
             }
         }
     }
