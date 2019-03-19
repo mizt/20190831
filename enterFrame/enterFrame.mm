3d3ccf7636e7980b30e73cbc84481fcc74de64d5
mizt <info@mizt.org>
2019-03-20 04:16:04 +0900

    updated

--- enterFrame/enterFrame.mm
+++ enterFrame/enterFrame.mm
@@ -11,13 +11,17 @@ class App {
         
         App() {
             this->timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER,0,0,dispatch_queue_create("ENTER_FRAME",0));
-            dispatch_source_set_timer(this->timer,dispatch_time(0,0),(1.0/120)*1000000000,0);
+            dispatch_source_set_timer(this->timer,dispatch_time(0,0),(1.0/60)*1000000000,0);
             dispatch_source_set_event_handler(this->timer,^{
                 double current = CFAbsoluteTimeGetCurrent();
-                NSLog(@"%f",current-this->then);
+                NSLog(@"ENTER_FRAME %f",current-this->then);
                 this->then = current;
             });
             if(this->timer) dispatch_resume(this->timer);
+        }
+        
+        void update(double time) {
+             NSLog(@"update %f",time);
         }
         
         ~App() {
@@ -28,11 +32,40 @@ class App {
         }
 };
 
-int main(int argc, char *argv[]) {
+@interface AppDelegate:NSObject <NSApplicationDelegate> {
+    App *m;
+    NSTimer *timer;
+    double then;
+}
+@end
 
-    @autoreleasepool {
-        App *app = new App();
-        [[NSApplication sharedApplication] run];       
-        
+@implementation AppDelegate
+-(void)applicationDidFinishLaunching:(NSNotification*)aNotification {
+    
+    m = new App();
+    then = CFAbsoluteTimeGetCurrent();
+    timer = [NSTimer scheduledTimerWithTimeInterval:1/60. target:self selector:@selector(update:) userInfo:nil repeats:YES];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSModalPanelRunLoopMode];
+    [[NSRunLoop currentRunLoop] addTimer:timer forMode:NSEventTrackingRunLoopMode];
+}
+-(void)applicationWillTerminate:(NSNotification *)aNotification {
+    if(timer&&[timer isValid]) [timer invalidate];
+    if(m) delete m;
+}
+-(void)update:(NSTimer*)timer {
+    if(m) {
+        double current = CFAbsoluteTimeGetCurrent();
+        m->update(current-then);
+        then = current;
     }
+}
+@end
+
+int main (int argc, const char * argv[]) {
+    id app = [NSApplication sharedApplication];
+    id delegat = [AppDelegate alloc];
+    [app setDelegate:delegat];
+    [app run];
+    return 0;
 }
