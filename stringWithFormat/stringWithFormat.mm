8304927ef8a1e8a21c0c77556bf9c5fa7500b5fb
mizt <info@mizt.org>
2019-04-17 00:53:58 +0900

    updated

--- stringWithFormat/stringWithFormat.mm
+++ stringWithFormat/stringWithFormat.mm
@@ -15,55 +15,60 @@ const char *stringWithFormat(const char *format, ...) {
 int main(int argc, char *argv[]) {
     @autoreleasepool {
         
-        NSLog(@"%s",stringWithFormat(R"(<!DOCTYPE html>
+        const char *html = stringWithFormat(R"(<!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title></title>
-    <style>
-    * {
-        margin:0;
-        padding:0;
+<meta charset="UTF-8">
+<title></title>
+<style>
+* {
+    margin:0;
+    padding:0;
+}
+body {
+    width: 100%%;
+    height: 100vh;
+    overflow: hidden;
+    font-family:'Hiragino Kaku Gothic Pro','ヒラギノ角ゴ Pro W3',Meiryo,メイリオ,Osaka,'MS PGothic',arial,helvetica,sans-serif;
+    //background-color:transparent;
+}
+div#current {
+    position:absolute;
+    width:95%%;
+    margin-left:2.5%%;
+    color:rgba(%d,%d,%d,%f);
+    font-size:54px;
+    font-weight:bold;
+    left:0;
+    top:50%%;
+    -webkit-transform: translateY(-50%%);
+    text-align:center;
+    line-height:1.4em;
+    word-break: break-all;
+    -webkit-font-smoothing:antialiased;
+    text-shadow:1.25px 0 0 rgba(22,22,22,0.4),0 1.25px 0 rgba(22,22,22,0.4),-1.25px 0 0 rgba(22,22,22,0.4),0 -1.25px 0 rgba(22,22,22,0.4);
+}
+
+</style>
+</head>
+<body>
+<div id="current"><p></p></div>
+<script>
+    window.current = function (str) {
+        var el = document.getElementById("current");
+        var p = el.querySelector("p");
+        p.textContent = str;
+        p.style.textAlign = (p.clientHeight>100)?"left":"center";
     }
-    body {
-        width:100%%;
-        height:100vh;
-        overflow:hidden;
-        background-color:transparent;
-    }
-    div#text {
-        position:absolute;
-        width:95%%;
-        margin-left:2.5%%;
-        color:rgba(%d,%d,%d,%f);
-        font-kerning:normal;
-        font-feature-settings:"palt";
-        font-family:"Hiragino Kaku Gothic Pro","ヒラギノ角ゴ Pro W3",sans-serif;
-        font-size:64px;
-        font-weight:bold;
-        left:0;
-        top:50%%;
-        -webkit-transform:translateY(-50%%);
-        text-align:center;
-        line-height:1.4em;
-        word-break:break-all;
-        -webkit-font-smoothing:antialiased;
-        text-shadow:1.25px 0 0 rgba(0,0,0,0.6),0 1.25px 0 rgba(0,0,0,0.6),-1.25px 0 0 rgba(0,0,0,0.6),0 -1.25px 0 rgba(0,0,0,0.6);
-    }
-    </style>
-    </head>
-    <body>
-        <div id="text"><p></p></div>
-        <script>
-            window.setText = function(str) {
-                var p = document.getElementById("text").querySelector("p");
-                p.textContent = str;
-                p.style.textAlign = (p.clientHeight>100)?"left":"center";
-            }
-        </script>
-    </body>
+</script>
+</body>
 </html>)",
-        255,255,255,0.85));
+        ((16777215>>16)&0xFF),
+        ((16777215>>8)&0xFF),
+        ((16777215)&0xFF),
+        0.85);
+                
+        NSLog(@"%s",html);
 
     }
 }
